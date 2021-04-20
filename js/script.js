7322d9e21c8cc5c8b3ef44259657c03c3c8bdbad
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-20 17:34:01 +0200

    operatore ternario prove

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const app = new Vue({
     data: {
         class1:'title',
         class2:'upper',
+        class3:'card',
         title: 'Welcome Vue', 
         image: 'https://cdn.pixabay.com/photo/2016/03/22/15/47/board-1273128_1280.jpg',
         titleInterface:'',
@@ -20,6 +21,29 @@ const app = new Vue({
         cognome:'',
         userName:'',
 
+    },
+
+    methods: {
+        sayHi(){
+            this.title = `Welcome ${this.nome}`; 
+            console.log(`Welcome ${this.nome}`);
+        },
+        changeColor(){
+            //A
+            // if(this.class3 === 'card'){
+            //     this.class3 = 'change'
+            // } else {
+            //     this.class3 = 'card';
+            // }
+
+            // B OPERATORE TERNARIO
+
+            /**
+             * classe di riferimento = condizione true ?(true o false) cambio : (else) 'cambio'
+             */
+
+            this.class3 = (this.class3 === 'card') ? 'change' : 'card';
+        }
     }
 });
     
