5fa7060b8582d29f17428d91e1660dfad024033a
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-04-20 14:46:29 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Vue', Vue);
+console.log('Vue', Vue); // Verifica Vue
+
+/**
+ * Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+ *
+ *  Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        myClasses:'title',
+        title: 'Welcome Vue', 
+        image: 'https://cdn.pixabay.com/photo/2016/03/22/15/47/board-1273128_1280.jpg',
+    }
+});
+    
+    
+    
+
+    
+
+
